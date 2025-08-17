d563f666248983f1956b33d788e92ae9e3452580
Low Qi Jun, Cheka <chekalowqijun@gmail.com>
2025-08-17 22:50:36 +0800

    Add Echo and Exit functionality for TooDoo

--- src/main/java/TooDoo.java
+++ src/main/java/TooDoo.java
@@ -1,16 +1,34 @@
+import java.util.Scanner;
+
 public class TooDoo {
     public static void main(String[] args) {
 
+        Scanner userInputScanner = new Scanner(System.in);
         String chatBotName = "TooDoo";
         String welcomeMessage = "____________________________________________________________\n" 
                                 + "How are you dooing! " + chatBotName + " at your service!\n"
                                 + "What would you like me too doo for you tooday?\n"
                                 +  "____________________________________________________________";
-        String exitMessage = "Toodles! Visit me again soon!\n" 
+        String exitMessage = "____________________________________________________________\n" 
+                             + "Toodles! Visit me again soon!\n" 
                              + "____________________________________________________________";
+        String userInput;
 
         System.out.println(welcomeMessage);
+
+        while (true) {
+            userInput = userInputScanner.nextLine();
+
+            if (userInput == "bye") {
+                break;
+            }
+
+            System.out.println("____________________________________________________________\n" 
+                             + userInput + "\n" 
+                             + "____________________________________________________________");
+        }
+
+        userInputScanner.close();
         System.out.println(exitMessage);
-        
     }
 }
