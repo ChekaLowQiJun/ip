d2a9e319df59a11d0cdc7da881eae27d00468923
Low Qi Jun, Cheka <chekalowqijun@gmail.com>
2025-08-17 22:15:29 +0800

    Rename Duke to TooDoo and add greeting and exit

---
+++ src/main/java/TooDoo.java
@@ -0,0 +1,16 @@
+public class TooDoo {
+    public static void main(String[] args) {
+
+        String chatBotName = "TooDoo";
+        String welcomeMessage = "____________________________________________________________\n" 
+                                + "How are you dooing! " + chatBotName + " at your service!\n"
+                                + "What would you like me too doo for you tooday?\n"
+                                +  "____________________________________________________________";
+        String exitMessage = "Toodles! Visit me again soon!\n" 
+                             + "____________________________________________________________";
+
+        System.out.println(welcomeMessage);
+        System.out.println(exitMessage);
+        
+    }
+}
