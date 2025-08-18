8b3e89361c233072156fe911b777da93b62f173a
Low Qi Jun, Cheka <chekalowqijun@gmail.com>
2025-08-18 21:08:21 +0800

    Edit and incorporate EmptyToException

--- src/main/java/EmptyToException.java
+++ src/main/java/EmptyToException.java
@@ -1,5 +1,5 @@
 public class EmptyToException extends Exception {
     public EmptyToException() {
-        
+        super("Goodness me! Does this event never end? Be sure to include the ending time.");
     }
 }
--- src/main/java/TooDoo.java
+++ src/main/java/TooDoo.java
@@ -65,6 +65,9 @@ public class TooDoo {
             } catch (EmptyFromException e) {
                 System.out.println(HORIZONTAL_LINE + e.getMessage() + "\n" 
                                     + HORIZONTAL_LINE);
+            } catch (EmptyToException e) {
+                System.out.println(HORIZONTAL_LINE + e.getMessage() + "\n" 
+                                    + HORIZONTAL_LINE);
             }
         }
     }
@@ -115,7 +118,7 @@ public class TooDoo {
         itemsInList++;
     }
 
-    public static void addEvent(String[] splitUserInput) throws EmptyDescriptionException, EmptyFromException {
+    public static void addEvent(String[] splitUserInput) throws EmptyDescriptionException, EmptyFromException, EmptyToException {
         taskList[itemsInList] = new Event(processEventString(splitUserInput)[0], processEventString(splitUserInput)[1], processEventString(splitUserInput)[2]);
         System.out.println(HORIZONTAL_LINE + "Aye aye captain! The following task has been added: \n" 
                             + taskList[itemsInList] + "\n" 
@@ -165,7 +168,7 @@ public class TooDoo {
         return deadlineOutput;
     }
 
-    public static String[] processEventString(String[] splitEventString) throws EmptyDescriptionException, EmptyFromException {
+    public static String[] processEventString(String[] splitEventString) throws EmptyDescriptionException, EmptyFromException, EmptyToException {
         String[] eventOutput = new String[3];
         StringBuilder description = new StringBuilder();
         StringBuilder from = new StringBuilder();
@@ -191,6 +194,8 @@ public class TooDoo {
             throw new EmptyDescriptionException();
         } else if (from.length() == 0) {
             throw new EmptyFromException();
+        } else if (to.length() == 0) {
+            throw new EmptyToException();
         }
 
         eventOutput[0] = description.deleteCharAt(description.length() - 1).toString();
