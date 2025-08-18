fc8d8caade6036de5716a0e40ec24d2dc0f40efe
Low Qi Jun, Cheka <chekalowqijun@gmail.com>
2025-08-18 13:16:32 +0800

    Abstract processUserInput logic

--- src/main/java/TooDoo.java
+++ src/main/java/TooDoo.java
@@ -7,71 +7,68 @@ public class TooDoo {
     private static final String CHAT_BOT_NAME = "TooDoo";
     private static final String HORIZONTAL_LINE = "____________________________________________________________\n";
 
-    public static String getWelcome() {
-        return HORIZONTAL_LINE + "How are you dooing! " 
-                + CHAT_BOT_NAME + " at your service!\n"
-                + "What would you like me too doo for you tooday?\n"
-                +  HORIZONTAL_LINE;
+    private static Task[] taskList = new Task[100];
+    private static int itemsInList = 0;
+
+    public static void getWelcome() {
+        System.out.println( HORIZONTAL_LINE + "How are you dooing! " 
+                            + CHAT_BOT_NAME + " at your service!\n"
+                            + "What would you like me too doo for you tooday?\n"
+                            +  HORIZONTAL_LINE);
     }
 
-    public static String getExit() {
-        return "____________________________________________________________\n" 
-                + "Toodles! Visit me again soon!\n" 
-                + "____________________________________________________________";
+    public static void getExit() {
+        System.out.println(HORIZONTAL_LINE + "Toodles! Visit me again soon!\n" 
+                            + HORIZONTAL_LINE);
     }
 
     public static String getUserInput() {
         return userInputScanner.nextLine();
     }
 
-    public static void main(String[] args) {
-                                
-        String userInput;
-        Task[] taskList = new Task[100];
-        int itemsInList = 0;
-        String[] splitUserInput;
+    public static void processUserInput() {
+         while (true) {
+            String userInput = TooDoo.getUserInput();
+            String[] splitUserInput = userInput.split(" ");
+            String keyword = splitUserInput[0];
 
-        System.out.println(getWelcome());
-
-        while (true) {
-            userInput = getUserInput();
-
-            if (userInput.equals("bye")) {
+            if (keyword.equals("bye")) {
                 break;
-            } else if (userInput.equals("list")) {
-                System.out.println("____________________________________________________________\n"
-                                    + "Presenting too you your task list:");
+            } else if (keyword.equals("list")) {
+                System.out.println(HORIZONTAL_LINE + "Presenting too you your task list:");
                 for (int i = 0;i < itemsInList;i++) {
                     System.out.println((i + 1) + "." + taskList[i]);
                 }
-                System.out.println("____________________________________________________________");
+                System.out.println(HORIZONTAL_LINE);
+            } else if (keyword.equals("mark")) {
+                taskList[Integer.parseInt(splitUserInput[1]) - 1].markAsDone();
+                    System.out.println(HORIZONTAL_LINE + "Good Job! You have completed this task:\n" 
+                                + taskList[Integer.parseInt(splitUserInput[1]) - 1] + "\n" 
+                                + HORIZONTAL_LINE);
+            } else if (keyword.equals("unmark")) {
+                taskList[Integer.parseInt(splitUserInput[1]) - 1].markAsNotDone();
+                System.out.println(HORIZONTAL_LINE + "It's okay! Let's finish it another time!\n" 
+                            + taskList[Integer.parseInt(splitUserInput[1]) - 1] + "\n" 
+                            + HORIZONTAL_LINE);
             } else {
-                splitUserInput = userInput.split(" ");
-
-                if (splitUserInput[0].equals("mark")) {
-                    taskList[Integer.parseInt(splitUserInput[1]) - 1].markAsDone();
-                    System.out.println("____________________________________________________________\n" 
-                                + "Good Job! You have completed this task:\n" 
-                                + taskList[Integer.parseInt(splitUserInput[1]) - 1] + "\n" 
-                                + "____________________________________________________________");
-                } else if (splitUserInput[0].equals("unmark")) {
-                    taskList[Integer.parseInt(splitUserInput[1]) - 1].markAsNotDone();
-                    System.out.println("____________________________________________________________\n" 
-                                + "It's okay! Let's finish it another time!\n" 
-                                + taskList[Integer.parseInt(splitUserInput[1]) - 1] + "\n" 
-                                + "____________________________________________________________");
-                } else {
-                    System.out.println("____________________________________________________________\n" 
-                                + "Added: " + userInput + "\n" 
-                                + "____________________________________________________________");
-                    
-                    taskList[itemsInList] = new Task(userInput);
-                    itemsInList++;
-                }
+                System.out.println(HORIZONTAL_LINE + "Added: " + userInput + "\n" 
+                            + HORIZONTAL_LINE);
+                
+                taskList[itemsInList] = new Task(userInput);
+                itemsInList++;
             }
         }
+    }
 
+    public static String getKeyWord(String[] splitUserInput) {
+        return splitUserInput[0];
+    }
+
+    public static void main(String[] args) {
+
+        TooDoo.getWelcome();
+        TooDoo.processUserInput();
+        TooDoo.getExit();
         userInputScanner.close();
-        System.out.println(getExit());
     }
 }
